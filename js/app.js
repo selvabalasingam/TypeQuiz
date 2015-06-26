784743d5dd62d7fc6a274b3b8c304687dfc3c37b
Selva Balasingam <selva.balasingam@gmail.com>
2015-06-25 20:21:37 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ var resetCounters = function () {
 }
 resetCounters();
 
-// Questions and answers
+// Questions and answers (Make each question text change font and buttons)
 function Font (minisculeA, loremText, answers, correct) {
     this.minisculeA = minisculeA;
     this.loremText = loremText;
@@ -23,7 +23,7 @@ var typeface = new Array(); //GARAMOND
 typeface[1] = new Font(
     ".garamond",
     ".garamond",
-    ["Verdana", "Garamond", "Helvetica"],
+    ["Verdana", "Garamond", "Monotype"],
     "Garamond"
 );
 console.log(typeface);
@@ -37,12 +37,65 @@ typeface[2] = new Font(
 );
 console.log(typeface);
 
+var typeface = new Array(); //TIMES
+typeface[3] = new Font(
+    ".times",
+    ".times",
+    ["Goudy Old Style", "Serifa", "Times"],
+    "Times"
+);
+console.log(typeface);
 
+var typeface = new Array(); //VERDANA
+typeface[4] = new Font(
+    ".verdana",
+    ".verdana",
+    ["Verdana", "Verdy", "Script"],
+    "Verdana"
+);
+console.log(typeface);
 
-// Make each question text change font and buttons
+var typeface = new Array(); //ARIAL
+typeface[5] = new Font(
+    ".arial",
+    ".arial",
+    ["Comic Sans", "Trade Gothic", "Arial"],
+    "Arial"
+);
+console.log(typeface);
+
 // Question Count updated per each question
+var guessNumber = function() {
+	counter += 1; 
+	$('.questionCount').text(counter);
+};
+
 // Score updated per each question
+var addPoints = function() {
+	score += 20; /* means counter = counter + amount; (increment by 20) */
+	$('.scorePercentage').text(score);
+};
+
+// Feedback
+var correct = function(text, color){ /*--- to change the feedback everytime user enters number ---*/
+	$(".feedback").text("Correct!");
+}
+
+var wrong = function(text, color){ /*--- to change the feedback everytime user enters number ---*/
+	$(".feedback").text("Wrong!");
+}
+
+var introLine = function(text, color){ /*--- to change the feedback everytime user enters number ---*/
+	$(".feedback").text("Let the games begin!");
+}
+
 // Restart button
+$(".restartGame").click(function(){
+	counter = 1;
+	score = 0;
+	introLine("Make a guess!"); //Replaces current header
+});
+
 // last page (empty) shows only final score + Final comment + Restart button (in big bold letters)
 
 
