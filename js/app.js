a0705d9a21b4ead3b1978a515b1f2c210ee84b09
Selva Balasingam <selva.balasingam@gmail.com>
2015-06-30 14:53:30 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,39 +1,28 @@
 $(document).ready(function(){
 
-// Begin counter at 1 and score at 0
-var counter;
-var score;
-var resetCounters = function () {
-    counter = 1; 
-    $('.questionCount').text(counter);
-    score = 0;
-    $('.scorePercentage').text(score);
-}
-resetCounters();
-
-////// beginning matthew //////
-
 var Quiz = function(questions) {
     this.questions = questions; // array of Questions.
 };
 
+// Begin counter at 1 and score at 0
 Quiz.prototype = {
     getScore: function() {
         var total = this.questions.length;
         var correct = this.getTotalCorrect();
-
-        return correct / total;
+        $('.scorePercentage').text(return correct / total * 100);
     },
     getTotalCorrect: function() {
         return this.questions.filter(function(q) {
             return q.isCorrect();
+        $('.questionCount').text(total);
         }).length;
     }
 };
 
-    
+// Formatting the questions
 var Question = function(options) {
-    this.question = options.question; // question
+    this.a = options.a; //transform font type for letter "a"
+    this.lorem = options.lorem; // transform font type for the lorem ipsum text
     this.choices = options.choices; // choices
     this.correctChoice = options.correctChoice; // this correct
     this.answer = null; // this what the user chooses
@@ -48,25 +37,49 @@ Question.prototype = {
     }
 };
 
-/////////////////////////////////
-var questions = [
-    new Question({
-        question: 'What font is this?',
-        choices: ['arial', 'times', 'helvetica'],
-        correct: 'arial'
-    }),
-    new Question({
-        question: 'What font is this?',
-        choices: ['arial', 'times', 'helvetica'],
-        correct: 'arial'
-    })
-    //, ...
+// Details for each question
+var typeface = new Array(); //GARAMOND
+typeface[0] = new Font(
+    ".garamond",
+    ".garamond",
+    ["Verdana", "Garamond", "Monotype"],
+    "Garamond"
+);
+console.log(typeface);
 
-];
-var quiz = new Quiz(questions);
+typeface[1] = new Font(  //HELVETICA
+    ".helvetica",
+    ".helvetica",
+    ["Baskerville", "Sans-serif", "Helvetica"],
+    "Helvetica"
+);
+console.log(typeface);
 
-/// buttons
+typeface[2] = new Font( //TIMES
+    ".times",
+    ".times",
+    ["Goudy Old Style", "Serifa", "Times"],
+    "Times"
+);
+console.log(typeface);
 
+typeface[3] = new Font( //VERDANA
+    ".verdana",
+    ".verdana",
+    ["Verdana", "Verdy", "Script"],
+    "Verdana"
+);
+console.log(typeface);
+
+typeface[4] = new Font( //ARIAL
+    ".arial",
+    ".arial",
+    ["Comic Sans", "Trade Gothic", "Arial"],
+    "Arial"
+);
+console.log(typeface);
+
+// buttons
 var choices = quiz.questions[0].choices;
 $('button.choice').each(function(i) {
     $(this).text(choices[i]);
@@ -75,64 +88,6 @@ $('button.choice').each(function(i) {
 $('button.choice').click(function(e) {
     var choice = $(e.target).text();
 });
-
-///// end matthew ///////
-
-var typeface = new Array(); //GARAMOND
-typeface[1] = new Font(
-    ".garamond",
-    ".garamond",
-    ["Verdana", "Garamond", "Monotype"],
-    "Garamond"
-);
-console.log(typeface);
-
-typeface[2] = new Font(  //HELVETICA
-    ".helvetica",
-    ".helvetica",
-    ["Baskerville", "Sans-serif", "Helvetica"],
-    "Helvetica"
-);
-console.log(typeface);
-
-var typeface = new Array(); //TIMES
-typeface[3] = new Font(
-    ".times",
-    ".times",
-    ["Goudy Old Style", "Serifa", "Times"],
-    "Times"
-);
-console.log(typeface);
-
-var typeface = new Array(); //VERDANA
-typeface[4] = new Font(
-    ".verdana",
-    ".verdana",
-    ["Verdana", "Verdy", "Script"],
-    "Verdana"
-);
-console.log(typeface);
-
-var typeface = new Array(); //ARIAL
-typeface[5] = new Font(
-    ".arial",
-    ".arial",
-    ["Comic Sans", "Trade Gothic", "Arial"],
-    "Arial"
-);
-console.log(typeface);
-
-// Question Count updated per each question
-var guessNumber = function() {
-    counter += 1; 
-    $('.questionCount').text(counter);
-};
-
-// Score updated per each question
-var addPoints = function() {
-    score += 20; /* means counter = counter + amount; (increment by 20) */
-    $('.scorePercentage').text(score);
-};
 
 // Feedback
 var correct = function(text, color){ /*--- to change the feedback everytime user enters number ---*/
@@ -149,12 +104,9 @@ var introLine = function(text, color){ /*--- to change the feedback everytime us
 
 // Restart button
 $(".restartGame").click(function(){
-    counter = 1;
-    score = 0;
+    total = 0;
+    correct = 0;
     introLine(); //Replaces current header
 });
-
-// last page (empty) shows only final score + Final comment + Restart button (in big bold letters)
-
 
 });
