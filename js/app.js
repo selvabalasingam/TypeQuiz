100b8b0e28de9183885a44266b55e86a9c0d5cdf
Selva Balasingam <selva.balasingam@gmail.com>
2015-07-01 13:51:11 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -2,27 +2,44 @@ $(document).ready(function(){
 
 var Quiz = function(questions) {
     this.questions = questions; // array of Questions.
+    this.current = 0;
 };
+//
+// $('.questionCount').text(total);
+// $('.scorePercentage').text(correct / total * 100);
 
 // Begin counter at 1 and score at 0
 Quiz.prototype = {
     getScore: function() {
         var total = this.questions.length;
         var correct = this.getTotalCorrect();
-        $('.scorePercentage').text(correct / total * 100);
+        return correct / total;
     },
     getTotalCorrect: function() {
         return this.questions.filter(function(q) {
             return q.isCorrect();
-        $('.questionCount').text(total);
         }).length;
+    },
+    getCurrentQuestion: function() {
+        return this.questions[this.current];
+    },
+    reset: function() {
+        this.current = 0;
+        this.questions.forEach(function(q) {
+            q.answer = null; 
+        });
+    },
+    getNextQuestion: function() {
+        // only increment the counter if there is another question
+        if (this.current < this.questions.length) {
+            this.current++;
+        }
+        return this.questions[this.current];
     }
 };
 
 // Questions
 var Question = function(options) {
-    this.a = options.a; //transform font type for letter "a"
-    this.lorem = options.lorem; // transform font type for the lorem ipsum text
     this.choices = options.choices; // choices
     this.correctChoice = options.correctChoice; // this correct
     this.answer = null; // this what the user chooses
@@ -37,56 +54,48 @@ Question.prototype = {
     }
 };
 
-// Details for each question
-var typeface = new Array(); //GARAMOND
-typeface[0] = new Font(
-    ".garamond",
-    ".garamond",
-    ["Verdana", "Garamond", "Monotype"],
-    "Garamond"
-);
-console.log(typeface);
-
-typeface[1] = new Font(  //HELVETICA
-    ".helvetica",
-    ".helvetica",
-    ["Baskerville", "Sans-serif", "Helvetica"],
-    "Helvetica"
-);
-console.log(typeface);
-
-typeface[2] = new Font( //TIMES
-    ".times",
-    ".times",
-    ["Goudy Old Style", "Serifa", "Times"],
-    "Times"
-);
-console.log(typeface);
-
-typeface[3] = new Font( //VERDANA
-    ".verdana",
-    ".verdana",
-    ["Verdana", "Verdy", "Script"],
-    "Verdana"
-);
-console.log(typeface);
-
-typeface[4] = new Font( //ARIAL
-    ".arial",
-    ".arial",
-    ["Comic Sans", "Trade Gothic", "Arial"],
-    "Arial"
-);
-console.log(typeface);
+var quiz = new Quiz([
+    new Question({
+        choices: ["Verdana", "Garamond", "Monotype"],
+        correctChoice: "Garamond"
+    }),
+    new Question({
+        choices: ["Baskerville", "Sans-serif", "Helvetica"],
+        correctChoice: "Helvetica"
+    }),
+    new Question({
+        choices: ["Goudy Old Style", "Serifa", "Times"],
+        correctChoice: "Times"
+    }),
+    new Question({
+        choices: ["Verdana", "Verdy", "Script"],
+        correctChoice: "Verdana"
+    }),
+    new Question({
+        choices: ["Comic Sans", "Trade Gothic", "Arial"],
+        correctChoice: "Arial"
+    })
+]);
 
 // buttons
-var choices = quiz.questions[0].choices;
-$('button.choice').each(function(i) {
-    $(this).text(choices[i]);
-}); 
+var renderChoices = function(question) {
+    $('button.choice').each(function(i) {
+        $(this).text(question.choices[i]);
+    });
+};
+
+var renderText = function(question) {
+    this.fontStyle = question.fontStyle; // need to add the font-face style to the div.text element 
+    this.loremText = question.loremText; // (we already have the lorem text there... and we can remove all those )
+    $(".text").text(question); // section tags you have in your html
+};
 
 $('button.choice').click(function(e) {
     var choice = $(e.target).text();
+    var question = quiz.getCurrentQuestion();
+    // set the answer on the question object
+    // then go to the next question
+    // then render the buttons and set the text font face
 });
 
 // Feedback
@@ -106,7 +115,11 @@ var introLine = function(text, color){ /*--- to change the feedback everytime us
 $(".restartGame").click(function(){
     total = 0;
     correct = 0;
+    // reset the quiz
     introLine(); //Replaces current header
 });
+
+// render the button choices and set the text font-face
+
 
 });
